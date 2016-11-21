ea23863fc5e768af9b76a6e10d7a40a37460f7e0
Sudha Parimala <bmwaudi247@gmail.com>
2016-11-21 11:19:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,18 +5,93 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+ articleOne : {
+	title: 'Article One | Sudha',
+	heading : 'Artile One',
+	date: 'Nov 20, 2016',
+	content:  ` <p>                                                                                                                                                                                                                                                                                                     
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+             <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+             <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p> `
+            
+},
+
+ articleTwo : {
+	title: 'Article Two | Sudha',
+	heading: 'Article Two',
+	date: 'Nov 20, 2016',
+	content: `
+      <p>Hi I am article two!</p>
+	`
+},
+
+ articleThree : {
+	title: 'Article Three | Sudha',
+	heading: 'Article Three',
+	date: 'Nov 20, 2016',
+	content: `
+      <p>Hi I am article three!</p>
+	`
+}
+
+};
+
+function createTemplate(data){
+	var title = data.title;
+	var heading = data.heading;
+	var date = data.date;
+	var content = data.content;
+var HtmlTemplate = `<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+    <link href="/ui/style.css" rel="stylesheet" /
+    
+     </head>
+    <body>
+        
+    <div class="container">   
+        <div>
+            <G href = "/">Home</G>
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}
+        </div>
+        </div> 
+    </body>
+</html>` ;
+return HtmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(reg,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   //res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articles.articleOne));
 });
 app.get('/article-two',function(reg,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+   //res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+   res.send(createTemplate(articles.articleTwo));
 });
-app.get('/article-three',function(reg,res){ 
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/article-three',function(reg,res){
+   //res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+   res.send(createTemplate(articles.articleThree));
 });
 
 app.get('/ui/style.css', function (req, res) {
@@ -26,12 +101,9 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/tic-tac-toe', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui/tic-tac-toe', 'tic.html'));
-});
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
-//Hello world!

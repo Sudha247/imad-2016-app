b2259f256567bfc19b2b6d291c1695ec4ed3368e
Sudha Parimala <bmwaudi247@gmail.com>
2016-11-22 19:32:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ var pool = new Pool(config);
 
 app.get('/test-db', function(req, res){
     //make a select statement and return a response
-    pool.query('SELECT * FROM test', function(err, result){
+    pool.query('SELECT * FROM articles', function(err, result){
         if(err){
             res.status(500).send(err.toString());
         }

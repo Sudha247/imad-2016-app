ee0cd1038b0cf81b3fb848945e4cd74698ede993
Sudha Parimala <bmwaudi247@gmail.com>
2016-11-25 01:43:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,6 +93,16 @@ app.get('/hash/:input',function(req, res){
     res.send(hashedString);
 });
 
+app.get('/create-user', function(req, res){
+    
+    var salt = crypto.getRandomBytes(128).toString('hex');
+    var dbString = hash(password, salt);
+    
+    pool.query('INSERT INTO "user" (username, password) values ($1, $2)', [username, dbString], function(err, result){
+        
+    });
+});
+
 /*app.get('/articles/:articleName', function(req, res){
     //articleName == article-one
     var articleName = req.params.articleName;

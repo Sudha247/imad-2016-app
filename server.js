b8e69bcaff6b1adac52ee474bb6ffb9cebce2c7f
Sudha Parimala <bmwaudi247@gmail.com>
2016-11-26 01:22:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -154,7 +154,7 @@ app.post('/login', function(req, res){
 });
 
 app.get('/check-login', function(req, res){
-   if(req.session&&req.session.auth &&req.session.auth.userId){
+   if(req.session && req.session.auth && req.session.auth.userId){
        res.send('You are logged in: '+req.seeion.auth.userId.toString());
    } 
    
